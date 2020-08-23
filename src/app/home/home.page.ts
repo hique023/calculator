c238b1fd252a5d5d934ed893c1fefdd60e5a55ab
Pedro Henrique Soares de Cam | Cappta <pedro.soares@cappta.com.br>
2020-08-23 11:21:57 -0300

    Commit da aplicação

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
+
+  public valor1: number;
+  public valor2: number;
+  public result;
+
+  public opMult() {
+    this. result = this.valor1 * this.valor2;
+  }
+
+  public opDiv() {
+    if (this.valor2 == 0 || this.valor2 == null) {
+      this.result = "Sem divisão";
+    } else {
+      this. result = this.valor1 / this.valor2
+    }
+  }
+
+  public opSom() {
+    this. result = this.valor1 + this.valor2
+  }
+
+  public opSub() {
+    this. result = this.valor1 - this.valor2
+  }
+
+  public setLimpa() {
+    this.valor1 = null;
+    this.valor2 = null;
+    this.result = null;
+  }
+
+  ngOnInit() {
+  }
 
 }
